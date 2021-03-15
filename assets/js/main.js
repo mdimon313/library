83ac97e9575388f11fe9b402ab6535319d623cf8
Mohammd Imon <imon26362@gmail.com>
2021-03-15 23:54:26 +0600

    update all file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,26 @@
+
 window.addEventListener("scroll", ()=> {
       const header = document.getElementById("header");
       const scrollPos = window.pageYOffset;
       
-      if (40 <= scrollPos) {
+      if (300 <= scrollPos) {
             header.classList.add("fixed");
       }
 
       if (0 >= scrollPos) {
             header.classList.remove("fixed");
       }
+});
+
+const open = document.getElementById("bars");
+
+const navigation = document.getElementById("nav");
+const close = document.querySelector(".hide");
+
+open.addEventListener("click", ()=> {
+	navigation.classList.add("show");
+});
+
+close.addEventListener("click", ()=> {
+	navigation.classList.remove("show");
 });
